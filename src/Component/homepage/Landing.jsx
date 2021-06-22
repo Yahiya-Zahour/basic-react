fe1bdc689e3c84fe70bc052a57cd3261635d3347
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-06-22 20:30:18 +0530

    pratice

---
+++ src/Component/homepage/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../A-Header/Header';
+const Landing = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+};
+export default Landing;
+
