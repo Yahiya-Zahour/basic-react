f36cae546359fde2a6b18e183c6fb223f4e54a4f
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-06-25 16:13:05 +0530

    practice

--- src/Component/A-Header/Header.jsx
+++ src/Component/A-Header/Header.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-// import {Navbar,Form,FormControl,Nav,Button,NavDropdown} from 'react-bootstrap';
-import {Button} from 'react-bootstrap'
+import {Navbar,Form,FormControl,Nav,Button,NavDropdown} from 'react-bootstrap';
+import "./Header.css";
+
 const Header = () => {
     return (
-        <div>
-     <Button variant="success">Success</Button>{' '}
-     <h1>hiiii</h1>
-        </div>
+        <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          
+          </Form>
+   
+            <Button id="1">Search</Button>
+        </Navbar.Collapse>
+      </Navbar>
     );
 };
 
