a0d39ee2267f88d43a089fdf84e2a3a1aae12cc1
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-09-22 16:20:42 +0530

    changes

--- src/Component/A-Header/Header.jsx
+++ src/Component/A-Header/Header.jsx
@@ -39,9 +39,7 @@ const Header = () => {
   </Navbar.Collapse>
 </Navbar>
 <button>login</button>
-<button>login</button>
-<button>login</button>
-<button>login</button>
+
 <button>login</button>
 
 
