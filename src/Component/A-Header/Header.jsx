ce1ed0e94db94d3867df65e1ee88fcb4e53bffef
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-09-29 16:16:06 +0530

    changes

--- src/Component/A-Header/Header.jsx
+++ src/Component/A-Header/Header.jsx
@@ -40,10 +40,7 @@ const Header = () => {
 </Navbar>
 <button>login</button>
 <button>login</button>
-<button>login</button>
-<button>login</button>
-<button>login</button>
-<button>login</button>
+
 
 
     </div>
