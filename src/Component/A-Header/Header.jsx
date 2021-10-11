a64d98e4e3c8e913f47c69f6a665a5d575d4d536
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-10-11 11:07:27 +0530

    practice

--- src/Component/A-Header/Header.jsx
+++ src/Component/A-Header/Header.jsx
@@ -1,48 +1,23 @@
 import React from "react";
 import "./Header.css";
-import { Navbar,NavDropdown,Nav,Form,FormControl,NavLink } from "react-bootstrap";
+import { Navbar,Nav,Container } from "react-bootstrap";
+
 const Header = () => {
   return (
     <div>
-     
-      <Navbar  >
-  <Navbar.Brand href="#"> <span>NAGUR INFRA </span></Navbar.Brand>
-  <Navbar.Toggle aria-controls="navbarScroll" />
-  <Navbar.Collapse id="navbarScroll">
-    <Nav
-      className="mr-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
-      navbarScroll
-    >
-      <NavLink to="/Home">Home</NavLink>
-      <NavLink to="/Service">Services</NavLink>
-      
-      <NavDropdown title="Contact us" id="navbarScrollingDropdown">
-        <NavDropdown.Item href="">Contact</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">About us</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action5">Our work done</NavDropdown.Item>
-      </NavDropdown>
-      <Nav.Link href="#" disabled>
-        <link rel="stylesheet" href="Service.jsx" />
-      </Nav.Link>
+      <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
     </Nav>
-    <Form className="d-flex">
-      <FormControl
-        type="search"
-        placeholder="Search"
-        className="mr-2"
-        aria-label="Search"
-      />
-    </Form>
-    
-  </Navbar.Collapse>
-</Navbar>
+    </Container>
+  </Navbar>
 
 
-
-
-
+<button>Done</button>
 
 
     </div>
