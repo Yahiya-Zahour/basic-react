7b32e5310fa1c66764dff00087158a3b9d993e9e
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-10-14 17:36:53 +0530

    practice

--- src/Component/A-Header/Header.jsx
+++ src/Component/A-Header/Header.jsx
@@ -14,10 +14,7 @@ const Header = () => {
 
 <button>login</button>
 <button>login</button>
-<button>login</button>
-<button>login</button> 
-<button>login</button>
-<button>login</button>
+
 
     </div>
   );
