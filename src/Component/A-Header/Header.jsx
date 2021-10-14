ed8d187da99cd3a148595d08f7b20cb33efd6266
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-10-14 17:35:04 +0530

    practice

--- src/Component/A-Header/Header.jsx
+++ src/Component/A-Header/Header.jsx
@@ -12,11 +12,8 @@ const Header = () => {
      <a href="/">Contact</a>
       </nav>
 
-<button>login</button>
-<button>login</button>
-<button>login</button>
-<button>login</button>
-<button>login</button>
+      <button>login</button>
+
 <button>login</button>
 
     </div>
